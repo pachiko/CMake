af1d133fe51ca8bc220d28618eafe36d9ce36df0
pachiko <phillippang1994@gmail.com>
2024-04-02 21:46:25 +0800

    Step 2
    Creating a Library
    Adding an Option

--- Help/guide/tutorial/Step2/MathFunctions/MathFunctions.cxx
+++ Help/guide/tutorial/Step2/MathFunctions/MathFunctions.cxx
@@ -3,13 +3,21 @@
 // TODO 11: include cmath
 
 // TODO 10: Wrap the mysqrt include in a precompiled ifdef based on USE_MYMATH
+#ifdef USE_MYMATH
 #include "mysqrt.h"
+#else
+#include <cmath>
+#endif
 
 namespace mathfunctions {
-double sqrt(double x)
-{
-  // TODO 9: If USE_MYMATH is defined, use detail::mysqrt.
-  // Otherwise, use std::sqrt.
-  return detail::mysqrt(x);
-}
+	double sqrt(double x)
+	{
+		// TODO 9: If USE_MYMATH is defined, use detail::mysqrt.
+		// Otherwise, use std::sqrt.
+		#ifdef USE_MYMATH
+		return detail::mysqrt(x);
+		#else
+		return std::sqrt(x);
+		#endif
+	}
 }
