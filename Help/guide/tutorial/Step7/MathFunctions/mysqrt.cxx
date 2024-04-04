c449caba390a7f67585de3d055f13c6fb372052f
pachiko <phillippang1994@gmail.com>
2024-04-04 11:12:42 +0800

    Step 7
    Assessing Dependency Availability

--- Help/guide/tutorial/Step7/MathFunctions/mysqrt.cxx
+++ Help/guide/tutorial/Step7/MathFunctions/mysqrt.cxx
@@ -1,5 +1,6 @@
 #include "mysqrt.h"
 
+#include <cmath>
 #include <iostream>
 
 namespace mathfunctions {
@@ -12,11 +13,12 @@ double mysqrt(double x)
   }
 
   // TODO 5: If both HAVE_LOG and HAVE_EXP are defined,  use the following:
-  //// double result = std::exp(std::log(x) * 0.5);
-  //// std::cout << "Computing sqrt of " << x << " to be " << result
-  ////        << " using log and exp" << std::endl;
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+   double result = std::exp(std::log(x) * 0.5);
+   std::cout << "Computing sqrt of " << x << " to be " << result
+         << " using log and exp" << std::endl;
   // else, use the existing logic.
-
+#else
   // Hint: Don't forget the #endif before returning the result!
 
   double result = x;
@@ -30,7 +32,7 @@ double mysqrt(double x)
     result = result + 0.5 * delta / result;
     std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
   }
-
+#endif
   return result;
 }
 }
